181371390eb02ac3159a1b86bbf5592f2edffbd3
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-13 14:48:27 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src="" alt="Selected" className="selected" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
