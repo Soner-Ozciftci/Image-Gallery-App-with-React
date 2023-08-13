1a45eee3d7587016f63dca7338dc0eeebc6a7ce0
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-13 15:58:34 +0200

    onClick

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./style.css";
 
 function App() {
 
-const [selectedImg , setSelectedImg] = useState(Images[0]);
+  const [selectedImg, setSelectedImg]=useState(Images[0]);
 
   return (
     <div className="App">
       <div className="container">
-        <img src="" alt="Selected" className="selected" />
+        <img src={selectedImg} alt="Selected" className="selected" />
       </div>
       <div className="imgContainer">
         {Images.map((img, index) => (
-          <img key={index} src={img} alt="Bursa Lezzetleri" />
-        ))}
+          <img key={index} src={img} alt="Bursa Lezzetleri" 
+      onClick={()=> setSelectedImg}
+      />
+      ))}
       </div>
     </div>
   );
