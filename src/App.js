4353ab72e8e91ccc201e8ea39b9e03495bfe2b85
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-13 14:57:14 +0200

    img add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+
+
 function App() {
   return (
     <div className="App">
       <div className="container">
         <img src="" alt="Selected" className="selected" />
+      </div>
+      <div className="imgContainer">
+        <img src="" alt="Bursa lezzetleri"/>
       </div>
     </div>
   );
---
+++ src/Images.js
@@ -0,0 +1,7 @@
+export default[
+    https://i.lezzet.com.tr/images-xxlarge-recipe/how-to-make-iskender-kebab-homemade-iskender-kebab-recipe-bae78de4-7c4c-4d2d-9016-456852c22f5c.jpg
+    https://cdn.yemek.com/mnresize/940/940/uploads/2015/06/inegol-kofte-yemekcom.jpg
+//cdn.yemek.com/mnresize/940/940/uploads/2017/09/cantik-tarifi.jpg
+https://i.ytimg.com/vi/BOGjH4U4z0w/maxresdefault.jpg
+https://yemek.com/tarif/kemalpasa-tatlisi/
+]
